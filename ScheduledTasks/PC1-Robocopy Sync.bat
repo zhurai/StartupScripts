01096c39fc962aa3fc55b40734276235d47528a8
zhurai <zhurai.tsuki@gmail.com>
2022-06-08 14:58:52 -0700

    chore(schtasks): touchup comments

--- ScheduledTasks/PC1-Robocopy Sync.bat
+++ ScheduledTasks/PC1-Robocopy Sync.bat
@@ -1,4 +1,4 @@
-:: Sync Autoit Startup Automation Scripts
+:: Sync Autoit Startup Automation Scripts to GDrive
 ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\all" "D:\GoogleDrive\Personal Sync\Scripts-Autoit\Startup\all" /MIR /R:x /W:x
 ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\pc1" "D:\GoogleDrive\Personal Sync\Scripts-Autoit\Startup\pc1" /MIR /R:x /W:x
 ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\pc2" "D:\GoogleDrive\Personal Sync\Scripts-Autoit\Startup\pc2" /MIR /R:x /W:x
